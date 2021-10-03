5fb9a7f6dcea1c216512c57d68a8190761ffde03
ColdPotatoes <60796873+ColdPotatoes@users.noreply.github.com>
2021-10-02 20:25:07 -0400

    here you go clara

--- src/tindr/tScript.js
+++ src/tindr/tScript.js
@@ -1,10 +1,17 @@
-function removeElement() {
-    document.getElementById("pg1").style.display = "none";
-}
-function resetElement() {
-    document.getElementsByClassName("").style.visibility = "hidden";
-}
-function changeT() {
-    document.getElementById("").innerHTML = "";
-}
+        /*var texting = document.getElementbyId("words");*/
 
+        function transforming() {
+            console.log("yessir");
+            document.getElementById('words').style.transform = "translate(20%, 20%)";
+
+        }
+
+        function removeElement() {
+            document.getElementById('pg1').style.display = "none";
+        }
+        function resetElement() {
+            document.getElementsByClassName("").style.visibility = "hidden";
+        }
+        function changeT() {
+            document.getElementById("").innerHTML = "";
+        }
