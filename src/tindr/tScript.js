30914aab77a31d50158191c373c1ddacdb07a6c3
clarajeon <clarajeon126@gmail.com>
2021-10-02 22:13:12 -0400

    fixed :)))

--- src/tindr/tScript.js
+++ src/tindr/tScript.js
@@ -1,17 +1,37 @@
-        /*var texting = document.getElementbyId("words");*/
+const txt = require('txt.json');
 
-        function transforming() {
-            console.log("yessir");
-            document.getElementById('words').style.transform = "translate(20%, 20%)";
+var i = 0
 
-        }
+function transforming() {
 
-        function removeElement() {
-            document.getElementById('pg1').style.display = "none";
-        }
-        function resetElement() {
-            document.getElementsByClassName("").style.visibility = "hidden";
-        }
-        function changeT() {
-            document.getElementById("").innerHTML = "";
-        }
+    console.log(i)
+    //change text
+    document.getElementById("words").innerHTML = txt[i]
+
+    if(i % 2 == 0){
+        //flip image
+        document.getElementById('astro-img').style.transform = "scaleX(-1)";
+
+        //flip text and image
+        var content = document.getElementById('astro-img');
+        var parent = content.parentNode;
+        parent.insertBefore(content, parent.firstChild);
+
+        //show date picker
+        document.getElementById('start').style.display ="inline-block"
+        
+    }
+
+    console.log("yessir");
+
+    i++
+}
+function removeElement() {
+    document.getElementById('pg1').style.display = "none";
+}
+function resetElement() {
+    document.getElementsByClassName("").style.visibility = "hidden";
+}
+function changeT() {
+    document.getElementById("").innerHTML = "";
+}
