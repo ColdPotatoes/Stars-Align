5552ab3bddb7cf5a948c3478fd7c5698d7a9d928
ColdPotatoes <60796873+ColdPotatoes@users.noreply.github.com>
2021-10-02 20:06:33 -0400

    all changes

--- src/starting/sScript.js
+++ src/starting/sScript.js
@@ -2,6 +2,17 @@ function btnTindr() {
     window.location='https://cornhub.website/';
 }
 
+let data = {
+    'address': "1600 Pennsylvania Avenue NW",
+    'key': 'AIzaSyAxD0n3nubD6HZ9oZjuyer_vfJ3oWiSZq0',
+}
+
+async function getCoords() {
+    const res = await fetch(`https://maps.googleapis.com/maps/api/geocode/json?${new URLSearchParams( data ).toString()}`);
+    let obj = await res.json();
+    console.log(obj);
+}
+
 function btnOrbit() {
     window.location='http://www.ornnhub.net';
 }
---
+++ src/tindr/tScript.js
@@ -0,0 +1,10 @@
+function removeElement() {
+    document.getElementById("pg1").style.display = "none";
+}
+function resetElement() {
+    document.getElementsByClassName("").style.visibility = "hidden";
+}
+function changeT() {
+    document.getElementById("").innerHTML = "";
+}
+
